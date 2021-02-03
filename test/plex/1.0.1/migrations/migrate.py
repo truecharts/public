9bcb41d9d549c7a04459423f87487fe32e494696
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-02-03 18:11:31 +0500

    Update plex migration to use updateStrategy

--- test/plex/1.0.1/migrations/migrate.py
+++ test/plex/1.0.1/migrations/migrate.py
@@ -1,16 +1,19 @@
 def migrate(values):
-    values['appVolumeMounts'] = {
-        'transcode': {
-            'hostPathEnabled': values['transcodeHostPathEnabled'],
-            **({'hostPath': values['transcodeHostPath']} if values.get('transcodeHostPath') else {})
+    values.update({
+        'appVolumeMounts': {
+            'transcode': {
+                'hostPathEnabled': values['transcodeHostPathEnabled'],
+                **({'hostPath': values['transcodeHostPath']} if values.get('transcodeHostPath') else {})
+            },
+            'config': {
+                'hostPathEnabled': values['configHostPathEnabled'],
+                **({'hostPath': values['configHostPath']} if values.get('configHostPath') else {})
+            },
+            'data': {
+                'hostPathEnabled': values['dataHostPathEnabled'],
+                **({'hostPath': values['dataHostPath']} if values.get('dataHostPath') else {})
+            },
         },
-        'config': {
-            'hostPathEnabled': values['configHostPathEnabled'],
-            **({'hostPath': values['configHostPath']} if values.get('configHostPath') else {})
-        },
-        'data': {
-            'hostPathEnabled': values['dataHostPathEnabled'],
-            **({'hostPath': values['dataHostPath']} if values.get('dataHostPath') else {})
-        },
-    }
+        'updateStrategy': values.get('strategyType', 'Recreate'),
+    })
     return values
