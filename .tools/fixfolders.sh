2503ac66c1f4a50598bcb0d2f32c04a7ebd744b7
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-02-20 14:48:04 +0100

    Update Helm chart common to v1.2.0 - Part 2 (#155)
    - remove split1 content
    - Update-Folder-Names-and-Dependencies
    - Mixed tweaks
    - Add setPermissions to test_values.yaml
    - Update questions.yaml
    - Update questions.yaml
    - Don't set permission on USB devices (#143)
    - Remove nodePorts from zwavejs2mqtt because invalid ports (and not needed for test)
    
    Co-authored-by:  Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
    Co-authored-by:  Troy Prelog <35702532+tprelog@users.noreply.github.com>

---
+++ .tools/fixfolders.sh
@@ -0,0 +1,13 @@
+for chart in charts/*; do
+  if [ -d "${chart}" ]; then
+      maxfolderversion=$(ls -l ${chart} | grep ^d | awk '{print $9}' | tail -n 1)
+      maxchartversion=$(cat ${chart}/${maxfolderversion}/Chart.yaml | grep "^version: " | awk -F" " '{ print $2 }')
+      chartname=$(basename ${chart})
+      echo "Processing: ${chart} - folder: ${maxfolderversion} - version: ${maxchartversion}"
+      helm dependency update ${chart}/${maxfolderversion}
+      if [ "${maxfolderversion}" != "${maxchartversion}" ]; then
+          mv -f ${chart}/${maxfolderversion} ${chart}/${maxchartversion}
+          echo "renamed ${chart}/${maxfolderversion} to ${chart}/${maxchartversion}"
+      fi
+  fi
+done
