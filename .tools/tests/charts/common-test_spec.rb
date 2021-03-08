7761ce7bdf0ab651c45d1e3b3f39880148a26f0d
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-03-08 14:32:58 +0100

    Run as PUID/PGID by default and add startAsRoot option (#246)

--- .tools/tests/charts/common-test_spec.rb
+++ .tools/tests/charts/common-test_spec.rb
@@ -38,6 +38,19 @@ class Test < ChartTest
       end
     end
 
+    describe 'startAsRoot' do
+      it 'defaults to false = runAs 568' do
+        jq('.spec.template.spec.securityContext.runAsUser', resource('Deployment')).must_equal 568
+        jq('.spec.template.spec.securityContext.runAsGroup', resource('Deployment')).must_equal 568
+      end
+
+      it 'can be enabled = runAs nil' do
+        chart.value startAsRoot: true
+        jq('.spec.template.spec.securityContext.runAsUser', resource('Deployment')).must_equal nil
+        jq('.spec.template.spec.securityContext.runAsGroup', resource('Deployment')).must_equal nil
+      end
+    end
+
     describe 'hostNetwork' do
       it ' hostnetworking default = nil' do
         jq('.spec.template.spec.hostNetwork', resource('Deployment')).must_equal nil
