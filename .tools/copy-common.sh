665dd7490126d652ad83bd6ce0138a0804b4d068
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-04-05 18:16:27 +0200

    Initial Postgres-Operator setup

--- .tools/copy-common.sh
+++ .tools/copy-common.sh
@@ -5,32 +5,19 @@ for chart in stable/*; do
       chartname=$(basename ${chart})
       echo "Processing: ${chart} - folder: ${maxfolderversion} - version: ${maxchartversion}"
 
-	   rm -Rf ${chart}/${maxfolderversion}/charts/*.tgz
+	   rm -Rf ${chart}/${maxfolderversion}/charts/common*.tgz
        cp -f library/common-test/charts/* ${chart}/${maxfolderversion}/charts/
 
   fi
 done
 
-
-for chart in beta/*; do
+for chart in incubator/*; do
   if [ -d "${chart}" ]; then
       maxfolderversion=$(ls -l ${chart} | grep ^d | awk '{print $9}' | tail -n 1)
       chartname=$(basename ${chart})
       echo "Processing: ${chart} - folder: ${maxfolderversion} - version: ${maxchartversion}"
 
-	   rm -Rf ${chart}/${maxfolderversion}/charts/*.tgz
-       cp -f library/common-test/charts/* ${chart}/${maxfolderversion}/charts/
-
-  fi
-done
-
-for chart in staging/*; do
-  if [ -d "${chart}" ]; then
-      maxfolderversion=$(ls -l ${chart} | grep ^d | awk '{print $9}' | tail -n 1)
-      chartname=$(basename ${chart})
-      echo "Processing: ${chart} - folder: ${maxfolderversion} - version: ${maxchartversion}"
-
-	   rm -Rf ${chart}/${maxfolderversion}/charts/*.tgz
+	   rm -Rf ${chart}/${maxfolderversion}/charts/common*.tgz
        cp -f library/common-test/charts/* ${chart}/${maxfolderversion}/charts/
 
   fi
