abaddf597f1acc65b1a2b27da20a52e7e367895e
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-04-10 13:52:56 +0200

    Add dev train and excluded common-test chart.lock

--- .tools/copy-common.sh
+++ .tools/copy-common.sh
@@ -22,3 +22,15 @@ for chart in incubator/*; do
 
   fi
 done
+
+for chart in dev/*; do
+  if [ -d "${chart}" ]; then
+      maxfolderversion=$(ls -l ${chart} | grep ^d | awk '{print $9}' | tail -n 1)
+      chartname=$(basename ${chart})
+      echo "Processing: ${chart} - folder: ${maxfolderversion} - version: ${maxchartversion}"
+
+	   rm -Rf ${chart}/${maxfolderversion}/charts/*.tgz
+       cp -f library/common-test/charts/* ${chart}/${maxfolderversion}/charts/
+
+  fi
+done
