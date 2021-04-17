ba65fa983546580b668d89d4789c2c70f1e1bfd1
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-04-17 23:18:47 +0200

    Copy /app to /data and run from there

--- .containers/apps/youtubedl/entrypoint.sh
+++ .containers/apps/youtubedl/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-rm -R /data/*
-cp -Rf /app/* /data
+echo "Patching youtubedl-material"
+cd /data
+find . -maxdepth 1 ! -name users ! -name appdata  -exec rm -Rf {} +
+cp -Rn /app/* /data
 exec node /data/app.js ${@} ${EXTRA_ARGS}
