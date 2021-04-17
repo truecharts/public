79c62eb94e350a310b86d842aeff4b8cb21a83f3
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-04-17 20:09:06 +0200

    Fix version getting mistake with youtubedl

--- .containers/apps/youtubedl/latest-version.sh
+++ .containers/apps/youtubedl/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET "https://api.github.com/reposTzahi12345/YoutubeDL-Material/releases" | jq --raw-output '.[0].tag_name')
+version=$(curl -sX GET "https://api.github.com/repos/Tzahi12345/YoutubeDL-Material/releases" | jq --raw-output '.[0].tag_name')
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
