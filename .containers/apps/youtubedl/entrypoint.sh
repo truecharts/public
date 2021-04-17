6ee076711dcc2efe67a9536907cbd60c02330e46
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-04-17 23:57:22 +0200

    youtubedl ignore irrelevant errors

--- .containers/apps/youtubedl/entrypoint.sh
+++ .containers/apps/youtubedl/entrypoint.sh
@@ -2,6 +2,6 @@
 
 echo "Patching youtubedl-material"
 cd /data
-find . -maxdepth 1 ! -name users ! -name appdata  -exec rm -Rf {} +
-cp -Rn /app/* /data
+find . -maxdepth 1 ! -name users ! -name appdata  -exec rm -Rf {} + || echo""
+cp -Rn /app/* /data || echo""
 exec node /data/app.js ${@} ${EXTRA_ARGS}
