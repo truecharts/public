6a158aeb0ab41a747bdce78892fb25ffb0e85fe6
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-04-18 15:18:17 +0200

    own appfolder content by apps:apps user for youtubedl container (and remove youtubedltest)

--- .containers/apps/youtubedltest/latest-version.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-version=$(curl -sX GET "https://api.github.com/repos/Tzahi12345/YoutubeDL-Material/releases" | jq --raw-output '.[0].tag_name')
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
