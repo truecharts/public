abec79331812daee7e2ed09404aa7d1361e42981
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-04-18 16:47:58 +0200

    SHOOT I f'ed up

--- .containers/apps/youtubedl/entrypoint.sh
+++ .containers/apps/youtubedl/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-echo "Patching youtubedl-material"
-cd /data
-find . -maxdepth 1 ! -name users ! -name appdata  -exec rm -Rf {} + || echo""
-cp -Rn /app/* /data || echo""
-exec node /data/app.js ${@} ${EXTRA_ARGS}
+
+exec node /app/app.js ${@} ${EXTRA_ARGS}
