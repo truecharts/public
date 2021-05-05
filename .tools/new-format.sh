d841766b818975475f7114a2ae256c7c6c132871
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-05-06 00:30:00 +0200

    polish tooling

--- .tools/new-format.sh
+++ .tools/new-format.sh
@@ -4,14 +4,19 @@ for chart in stable/*; do
     maxfolderversion=$(ls -l ${chart} | grep ^d | awk '{print $9}' | tail -n 1)
     chartname=$(basename ${chart})
     path="${chart}/${maxfolderversion}"
-	mkdir -p ${path}/SCALE
+	rm -rf ${chart}/SCALE
+	rm -rf ${chart}/charts
+	rm -rf ${path}/charts
+	rm -rf ${path}/Chart.lock
     rm -rf ${path}/values.yaml
 	rm -rf app-readme.md
+	mkdir -p ${path}/SCALE
     mv ${chart}/item.yaml ${path}/SCALE/item.yaml
     mv ${path}/ix_values.yaml ${path}/SCALE/ix_values.yaml
     mv ${path}/questions.yaml ${path}/SCALE/questions.yaml
     mv ${path}/test_values.yaml ${path}/values.yaml
 	mv ${path}/* ${chart}/
+	mv ${path}/.helmignore ${chart}/.helmignore || echo ".helmignore not found"
 	rm -rf ${path}
   fi
 done
