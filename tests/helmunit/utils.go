72d164f3960436093d01d00d5ed6d43bc2e4e256
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-05-19 17:59:27 +0200

    add general shizzle

---
+++ tests/helmunit/utils.go
@@ -0,0 +1,37 @@
+package helmunit
+
+import (
+    "github.com/Jeffail/gabs"
+    "sigs.k8s.io/yaml"
+)
+
+func mergeMaps(a, b map[string]interface{}) map[string]interface{} {
+    out := make(map[string]interface{}, len(a))
+    for k, v := range a {
+        out[k] = v
+    }
+    for k, v := range b {
+        if v, ok := v.(map[string]interface{}); ok {
+            if bv, ok := out[k]; ok {
+                if bv, ok := bv.(map[string]interface{}); ok {
+                    out[k] = mergeMaps(bv, v)
+                    continue
+                }
+            }
+        }
+        out[k] = v
+    }
+    return out
+}
+
+func yamlToJson(yamlInput []byte) (jsonOutput *gabs.Container, err error) {
+    jsonBytes, err := yaml.YAMLToJSON(yamlInput)
+    if err != nil {
+        return nil, err
+    }
+    jsonParsed, err := gabs.ParseJSON(jsonBytes)
+    if err != nil {
+        return nil, err
+    }
+    return jsonParsed, nil
+}
