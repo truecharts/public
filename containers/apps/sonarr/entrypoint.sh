2ee1190fdac3ada99618d3cd1edde46cabc0bc79
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-05-30 23:22:13 +0200

    Cleanup and simplify containers and charts (#531)
    * move truecharts containers to k8s-at-home
    
    * Chart.yaml cleanup
    
    * bump patch versions

--- containers/apps/sonarr/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#shellcheck disable=SC1091
-source "/shim/umask.sh"
-source "/shim/vpn.sh"
-
-exec /usr/bin/mono --debug /app/Sonarr.exe --nobrowser --data=/config ${EXTRA_ARGS}
