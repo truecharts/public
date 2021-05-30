8b76a14c73b69a755c9e6bf72bc199646c9bd834
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-05-30 13:11:44 +0200

    remove ansible container for now

--- containers/apps/ansible/latest-version.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-version=$(curl -sX GET https://pypi.org/pypi/ansible/json | jq --raw-output '.["releases"] | keys[]' | grep -v rc | grep -v a | grep -v b | tail -n1rep -v buildcache | tail -n1)
-version="${version#*v}"
-version="${version#*release-}"
-echo "${version}"
