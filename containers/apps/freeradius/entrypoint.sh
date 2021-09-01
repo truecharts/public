a09829bac90c9a053c8bfb1ebabd2446eb2daa82
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-09-01 13:36:10 +0200

    Some radius fixes

--- containers/apps/freeradius/entrypoint.sh
+++ containers/apps/freeradius/entrypoint.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 set -e
 
-if [ -f "/config/clients.conf" ]; then
+if [ -f "/etc/raddb/clients.conf" ]; then
   echo "Radius config already exists, skipping config copy..."
 else
   echo "Radius config does not exist, copying..."
-  cp -Rf /etc/freeradius/* /config/
+  cp -Rf /etc/freeradius/* /etc/raddb/
 fi
 
-chown -R freerad:freerad /config
+chown -R freerad:freerad /etc/raddb/
 
 # this if will check if the first argument is a flag
 # but only works if all arguments require a hyphenated flag
 # -v; -SL; -f arg; etc will work, but not arg1 arg2
 if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
-    set -- freeradius -d /config "$@"
+    set -- freeradius "$@"
 fi
 
 # check for the expected command
 if [ "$1" = 'freeradius' ]; then
     shift
-    exec freeradius -f -d /config  "$@"
+    exec freeradius -f "$@"
 fi
 
 # many people are likely to call "radiusd" as well, so allow that
 if [ "$1" = 'radiusd' ]; then
     shift
-    exec freeradius -f -d /config  "$@"
+    exec freeradius -f "$@"
 fi
 
 # else default to run whatever the user wanted like "bash" or "sh"
