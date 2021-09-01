e3548ca2012cb86ce036e09073f42e819fc8ebc3
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-09-01 11:50:16 +0200

    raddb -> freeradius

--- containers/apps/freeradius/entrypoint.sh
+++ containers/apps/freeradius/entrypoint.sh
@@ -5,7 +5,7 @@ if [ -f "/config/clients.conf" ]; then
   echo "Radius config already exists, skipping config copy..."
 else
   echo "Radius config does not exist, copying..."
-  cp -Rf /etc/raddb/* /config/
+  cp -Rf /etc/freeradius/* /config/
 fi
 
 chown -R freerad:freerad /config
