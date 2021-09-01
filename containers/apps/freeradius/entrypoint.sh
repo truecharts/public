eb58df34ffc9103fa76d1fc17638518f842e86be
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-09-01 14:40:48 +0200

    fix freeradius container

--- containers/apps/freeradius/entrypoint.sh
+++ containers/apps/freeradius/entrypoint.sh
@@ -14,19 +14,19 @@ chown -R freerad:freerad /etc/raddb/
 # but only works if all arguments require a hyphenated flag
 # -v; -SL; -f arg; etc will work, but not arg1 arg2
 if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
-    set -- freeradius "$@"
+    set -- freeradius -d /etc/raddb "$@"
 fi
 
 # check for the expected command
 if [ "$1" = 'freeradius' ]; then
     shift
-    exec freeradius -f "$@"
+    exec freeradius -f -d /etc/raddb "$@"
 fi
 
 # many people are likely to call "radiusd" as well, so allow that
 if [ "$1" = 'radiusd' ]; then
     shift
-    exec freeradius -f "$@"
+    exec freeradius -f -d /etc/raddb "$@"
 fi
 
 # else default to run whatever the user wanted like "bash" or "sh"
