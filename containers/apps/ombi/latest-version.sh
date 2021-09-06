e2e75719e90c04bfbc38fd9e798f0b1614678a66
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-09-06 08:55:15 +0200

    add GOSS to sogo container and add version check scripting

--- containers/apps/ombi/latest-version.sh
+++ containers/apps/ombi/latest-version.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 version=$(curl -sX GET "https://api.github.com/repos/Ombi-app/Ombi/releases" | jq --raw-output '.[0].tag_name')
+version="${version#*Sogo-}"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
