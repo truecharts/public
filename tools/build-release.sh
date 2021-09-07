5af4f9b0e001fa8e0cf1e58a12091b9289bad92d
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-09-07 20:37:52 +0200

    Setup new test and release system (#919)
    * Tweak some release-build script parameters
    
    * adapt test to new release scripting
    
    * whoopsies
    
    * fix sh permissions
    
    * some fixes
    
    * use with
    
    * test bump
    
    * don't build indexes when running without production flag either
    
    * also validate the catalog
    
    * PWD/catalog
    
    * mtching
    "
    
    * Don't make change to primary repo when building catalog
    
    * Add doc generation feature
    
    * try fetch depth 100
    
    * remove helm-prep step to speedup test init
    
    * also generate general docs
    
    * adapt release workflow and some workflow cleanup

--- tools/build-release.sh
+++ tools/build-release.sh
@@ -31,22 +31,24 @@ Usage: $(basename "$0") <options>
     -o, --owner              The repo owner
     -r, --repo               The repo name
     -p, --production         Enables uploading releases to github releases
+    -s, --standalone         Disables Chart Releaser and Catalog Validation, for local generation
 EOF
 }
 
 main() {
     local version="$DEFAULT_CHART_RELEASER_VERSION"
     local config=
-    local charts_dir=charts
+    local standalone=
+    local charts_dir=charts/**
     local owner=
     local repo=
     local production=
     local charts_repo_url=
-    local token=${CR_TOKEN:-none}
+    local token=${CR_TOKEN:-false}
 
     parse_command_line "$@"
-    if [ "${token}" == "none" ]; then
-        echo "env CR_TOKEN not found, defaulting to production=false"
+    if [ "${token}" == "false" ]; then
+        echo "env #cr_TOKEN not found, defaulting to production=false"
         production="false"
     fi
 
@@ -61,9 +63,8 @@ main() {
     echo "Discovering changed charts since '$latest_tag'..."
     local changed_charts=()
     readarray -t changed_charts <<< "$(lookup_changed_charts "$latest_tag")"
-
+    copy_general_docs
     if [[ -n "${changed_charts[*]}" ]]; then
-        install_chart_releaser
 
         rm -rf .cr-release-packages
         mkdir -p .cr-release-packages
@@ -71,17 +72,20 @@ main() {
         rm -rf .cr-index
         mkdir -p .cr-index
 
+		prep_helm
         for chart in "${changed_charts[@]}"; do
             if [[ -d "$chart" ]]; then
                 chartversion=$(cat ${chart}/Chart.yaml | grep "^version: " | awk -F" " '{ print $2 }')
                 chartname=$(basename ${chart})
-                prep_app "${chart}"
+                train=$(basename $(dirname "$chart"))
+				generate_docs "$chart" "$chartname" "$train" "$chartversion"
+				copy_docs "$chart" "$chartname" "$train" "$chartversion"
                 helm dependency update "${chart}" --skip-refresh
                 package_chart "$chart"
                 if [[ -d "${chart}/SCALE" ]]; then
                   clean_apps "$chart" "$chartname" "$train" "$chartversion"
-                  patch_apps "$chart" "$chartname" "$train" "$chartversion"
                   copy_apps "$chart" "$chartname" "$train" "$chartversion"
+                  patch_apps "$chart" "$chartname" "$train" "$chartversion"
                 else
                   echo "Skipping chart ${chart}, no correct SCALE compatibility layer detected"
                 fi
@@ -89,10 +93,11 @@ main() {
                 echo "Chart '$chart' no longer exists in repo. Skipping it..."
             fi
         done
+        validate_catalog
         if [ "${production}" == "true" ]; then
         release_charts
-        fi
         update_index
+        fi
     else
         echo "Nothing to do. No chart changes detected."
     fi
@@ -100,12 +105,90 @@ main() {
     popd > /dev/null
 }
 
-prep_app() {
+copy_general_docs() {
+	yes | cp -rf index.yaml docs/index.yaml 2>/dev/null || :
+    yes | cp -rf .github/README.md docs/index.md 2>/dev/null || :
+    sed -i '1s/^/---\nhide:\n  - navigation\n  - toc\n---\n/' docs/index.md
+    sed -i 's~<!-- INSERT-DISCORD-WIDGET -->~<iframe src="https://discord.com/widget?id=830763548678291466\&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>~g' docs/index.md
+    yes | cp -rf .github/CODE_OF_CONDUCT docs/about/code_of_conduct.md 2>/dev/null || :
+    yes | cp -rf .github/CONTRIBUTING docs/development/contributing.md 2>/dev/null || :
+    yes | cp -rf .github/SUPPORT.md docs/manual/SUPPORT.md 2>/dev/null || :
+    yes | cp -rf LICENSE docs/about/legal/LICENSE.md 2>/dev/null || :
+    sed -i '1s/^/# License<br>\n\n/' docs/about/legal/LICENSE.md
+    yes | cp -rf NOTICE docs/about/legal/NOTICE.md 2>/dev/null || :
+    sed -i '1s/^/# NOTICE<br>\n\n/' docs/about/legal/NOTICE.md
+	}
+
+generate_docs() {
     local chart="$1"
-    echo "Creating app-readme.md for App: ${chartname}"
-    description=$(cat ${chart}/Chart.yaml | grep "^description: " | sed -r 's/^description: //')
-    echo "${description}" >> ${chart}/app-readme.md
-}
+    local chartname="$2"
+    local train="$3"
+    local chartversion="$4"
+	if [[ -z "$standalone" ]]; then
+         echo "Generating Docs"
+         if [ "${chartname}" == "common" ]; then
+             helm-docs \
+                 --ignore-file=".helmdocsignore" \
+                 --output-file="README.md" \
+                 --template-files="/home/runner/work/apps/apps/templates/docs/common-README.md.gotmpl" \
+                 --chart-search-root="/home/runner/work/apps/apps/charts/library/common"
+             helm-docs \
+                 --ignore-file=".helmdocsignore" \
+                 --output-file="helm-values.md" \
+                 --template-files="/home/runner/work/apps/apps/templates/docs/common-helm-values.md.gotmpl" \
+                 --chart-search-root="/home/runner/work/apps/apps/charts/library/common"
+	     else
+             helm-docs \
+                 --ignore-file=".helmdocsignore" \
+                 --output-file="README.md" \
+                 --template-files="/home/runner/work/apps/apps/templates/docs/README.md.gotmpl" \
+                 --chart-search-root="${chart}"
+             helm-docs \
+                 --ignore-file=".helmdocsignore" \
+                 --output-file="CONFIG.md" \
+                 --template-files="/home/runner/work/apps/apps/templates/docs/CONFIG.md.gotmpl" \
+                 --chart-search-root="${chart}"
+             helm-docs \
+                 --ignore-file=".helmdocsignore" \
+                 --output-file="app-readme.md" \
+                 --template-files="/home/runner/work/apps/apps/templates/docs/app-readme.md.gotmpl" \
+                 --chart-search-root="${chart}"
+             helm-docs \
+                 --ignore-file=".helmdocsignore" \
+                 --output-file="helm-values.md" \
+                 --template-files="/home/runner/work/apps/apps/templates/docs/helm-values.md.gotmpl" \
+                 --chart-search-root="${chart}"
+         fi
+	fi
+	}
+
+
+copy_docs() {
+    local chart="$1"
+    local chartname="$2"
+    local train="$3"
+    local chartversion="$4"
+	echo "Copying docs for: ${chart}"
+    if [ "${chartname}" == "common" ]; then
+	    mkdir -p docs/apps/common || :
+	    yes | cp -rf charts/library/common/README.md  docs/apps/common/index.md 2>/dev/null || :
+        yes | cp -rf charts/library/common/helm-values.md  docs/apps/common/helm-values.md 2>/dev/null || :
+	else
+        mkdir -p docs/apps/${train}/${chartname} || echo "app path already exists, continuing..."
+        yes | cp -rf ${chart}/README.md docs/apps/${train}/${chartname}/index.md 2>/dev/null || :
+        yes | cp -rf ${chart}/CONFIG.md docs/apps/${train}/${chartname}/CONFIG.md 2>/dev/null || :
+        yes | cp -rf ${chart}/helm-values.md docs/apps/${train}/${chartname}/helm-values.md 2>/dev/null || :
+        sed -i '1s/^/# License<br>\n\n/' docs/apps/${train}/${chartname}/LICENSE.md 2>/dev/null || :
+	fi
+	}
+
+prep_helm() {
+	if [[ -z "$standalone" ]]; then
+	helm repo add truecharts https://truecharts.org
+    helm repo add bitnami https://charts.bitnami.com/bitnami
+    helm repo update
+	fi
+	}
 
 clean_apps() {
     local chart="$1"
@@ -113,8 +196,8 @@ clean_apps() {
     local train="$3"
     local chartversion="$4"
     echo "Cleaning SCALE catalog for App: ${chartname}"
-    rm -Rf catalog/${train}/${chartname}/${chartversion} || true
-    rm -Rf catalog/${train}/${chartname}/item.yaml || true
+    rm -Rf catalog/${train}/${chartname}/${chartversion} 2>/dev/null || :
+    rm -Rf catalog/${train}/${chartname}/item.yaml 2>/dev/null || :
 }
 
 patch_apps() {
@@ -122,14 +205,15 @@ patch_apps() {
     local chartname="$2"
     local train="$3"
     local chartversion="$4"
+    local target="catalog/${train}/${chartname}/${chartversion}"
     echo "Applying SCALE patches for App: ${chartname}"
-    mv ${chart}/SCALE/item.yaml ${chart}/
-    mv ${chart}/SCALE/ix_values.yaml ${chart}/
-    mv ${chart}/SCALE/questions.yaml ${chart}/
-    cp -rf ${chart}/SCALE/templates/* ${chart}/templates || echo "ignoring templates directory..."
-    rm -rf ${chart}/SCALE
-    mv ${chart}/values.yaml ${chart}/test_values.yaml
-    touch ${chart}/values.yaml
+    mv ${target}/SCALE/ix_values.yaml ${target}/
+    mv ${target}/SCALE/questions.yaml ${target}/
+    cp -rf ${target}/SCALE/templates/* ${target}/templates 2>/dev/null || :
+    mv ${target}/SCALE/item.yaml catalog/${train}/${chartname}/item.yaml
+    rm -rf ${target}/SCALE
+    mv ${target}/values.yaml ${target}/test_values.yaml
+    touch ${target}/values.yaml
 }
 
 copy_apps() {
@@ -140,12 +224,14 @@ copy_apps() {
     echo "Copying App to Catalog: ${2}"
     mkdir -p catalog/${train}/${chartname}/${chartversion}
     cp -Rf ${chart}/* catalog/${train}/${chartname}/${chartversion}/
-    mv catalog/${train}/${chartname}/${chartversion}/item.yaml catalog/${train}/${chartname}/item.yaml
+
 }
 
 validate_catalog() {
+    if [[ -z "$standalone" ]]; then
     echo "Starting Catalog Validation"
-    /bin/bash -c "PWD=${pwd}; /usr/local/bin/catalog_validate validate --path $PWD/catalog"
+    /usr/local/bin/catalog_validate validate --path "${PWD}/catalog"
+    fi
 }
 
 
@@ -218,6 +304,9 @@ parse_command_line() {
             -p|--production)
                 production="true"
                 ;;
+            -s|--standalone)
+                standalone="true"
+                ;;
             *)
                 break
                 ;;
@@ -265,7 +354,7 @@ lookup_changed_charts() {
     local commit="$1"
 
     local changed_files
-    changed_files=$(git diff --find-renames --name-only "$commit" -- "$charts_dir")
+    changed_files=$(git diff --find-renames --name-only "$commit" -- "$charts_dir" | grep "Chart.yaml")
 
     local depth=$(( $(tr "/" "\n" <<< "$charts_dir" | sed '/^\(\.\)*$/d' | wc -l) + 1 ))
     local fields="1-${depth}"
@@ -275,14 +364,15 @@ lookup_changed_charts() {
 
 package_chart() {
     local chart="$1"
-
     local args=("$chart" --package-path .cr-release-packages)
     if [[ -n "$config" ]]; then
         args+=(--config "$config")
     fi
 
+    if [[ -z "$standalone" ]]; then
     echo "Packaging chart '$chart'..."
     cr package "${args[@]}"
+	fi
 }
 
 release_charts() {
@@ -291,8 +381,10 @@ release_charts() {
         args+=(--config "$config")
     fi
 
+    if [[ -z "$standalone" ]]; then
     echo 'Releasing charts...'
     cr upload "${args[@]}"
+    fi
 }
 
 update_index() {
@@ -301,8 +393,10 @@ update_index() {
         args+=(--config "$config")
     fi
 
+    if [[ -z "$standalone" ]]; then
     echo 'Updating charts repo index...'
     cr index "${args[@]}"
+	fi
 }
 
 main "$@"
