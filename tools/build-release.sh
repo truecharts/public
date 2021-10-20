849e6e02c511c4aa761bf41ffcdbc40f926006f3
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-10-20 10:14:56 +0200

    fix(CI): ensure Apps use the correct icon_url key in item.yaml

--- tools/build-release.sh
+++ tools/build-release.sh
@@ -398,6 +398,7 @@ patch_apps() {
     touch ${target}/values.yaml
     # Generate item.yaml
     cat ${target}/Chart.yaml | grep "icon" >> catalog/${train}/${chartname}/item.yaml
+    sed -i "s|^icon:|icon_url:|g" ${target}/chart.yaml
     echo "categories:" >> catalog/${train}/${chartname}/item.yaml
     cat ${target}/Chart.yaml | yq '.annotations."truecharts.org/catagories"' -r >> catalog/${train}/${chartname}/item.yaml
     # Generate SCALE App description file
