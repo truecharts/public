74a1b973db0d930365b9564ef1665965e8dd8b61
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-01-14 11:24:36 +0100

    fix a scripting mistake

--- tools/patchrc2.sh
+++ tools/patchrc2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-( wget -q -P /tmp https://github.com/truecharts/apps/raw/master/tools/RC2-HP1.deb && echo "download completed" || echo "download failed" ) && ( dpkg -i /root/python3-kubernetes-asyncio_20220112111117~truenas+1_all.deb && echo "patch completed" || echo "patch failed )" && rm -rf /tmp/RC2-HP1.deb && echo "Restarting Middleware..." && systemctl restart middlewared
+( wget -q -P /tmp https://github.com/truecharts/apps/raw/master/tools/RC2-HP1.deb && echo "download completed" || echo "download failed" ) && ( dpkg -i /root/python3-kubernetes-asyncio_20220112111117~truenas+1_all.deb && echo "patch completed" || echo "patch failed" ) && rm -rf /tmp/RC2-HP1.deb && echo "Restarting Middleware..." && systemctl restart middlewared
