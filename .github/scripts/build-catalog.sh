3feedd2a6570f45f76ee1952fa78f5ae63746188
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-07-19 13:53:56 +0200

    fix(ci): forgot to push a description change

--- .github/scripts/build-catalog.sh
+++ .github/scripts/build-catalog.sh
@@ -223,7 +223,7 @@ patch_apps() {
     # Generate SCALE App description file
     cat ${target}/Chart.yaml | yq .description -r >> ${target}/app-readme.md
     echo "" >> ${target}/app-readme.md
-    echo "This App is supplied by TrueCharts, for more information visit https://truecharts.org" >> ${target}/app-readme.md
+    echo "This App is supplied by TrueCharts, for more information visit the manual: https://truecharts.org/charts/${train}/${chartname}" >> ${target}/app-readme.md
     echo "" >> ${target}/app-readme.md
     echo "---" >> ${target}/app-readme.md
     echo "" >> ${target}/app-readme.md
