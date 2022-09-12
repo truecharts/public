2501b351d4c02dcf5070eb798c29b1f2b441f739
Stavros kois <s.kois@outlook.com>
2022-09-12 19:27:32 +0300

    chore(questions): split serviceSelector

--- .github/scripts/build-catalog.sh
+++ .github/scripts/build-catalog.sh
@@ -116,10 +116,30 @@ include_questions(){
     /# Include{serviceRoot}/ { for (i=0;i<n;++i) print a[i]; next }
     1' templates/questions/service/serviceRoot.yaml ${target}/questions.yaml > "tmp${chartname}" && mv "tmp${chartname}" ${target}/questions.yaml
 
-    # Replace # Include{serviceSelector} with the standard serviceSelector codesnippet
+    # Replace # Include{serviceSelectorExtras} with the standard serviceSelectorExtras codesnippet
     awk 'NR==FNR { a[n++]=$0; next }
-    /# Include{serviceSelector}/ { for (i=0;i<n;++i) print a[i]; next }
-    1' templates/questions/service/serviceSelector.yaml ${target}/questions.yaml > "tmp${chartname}" && mv "tmp${chartname}" ${target}/questions.yaml
+    /# Include{serviceSelectorExtras}/ { for (i=0;i<n;++i) print a[i]; next }
+    1' templates/questions/service/serviceSelectorExtras.yaml ${target}/questions.yaml > "tmp${chartname}" && mv "tmp${chartname}" ${target}/questions.yaml
+
+    # Replace # Include{serviceSelectorSimple} with the standard serviceSelectorSimple codesnippet
+    awk 'NR==FNR { a[n++]=$0; next }
+    /# Include{serviceSelectorSimple}/ { for (i=0;i<n;++i) print a[i]; next }
+    1' templates/questions/service/serviceSelectorSimple.yaml ${target}/questions.yaml > "tmp${chartname}" && mv "tmp${chartname}" ${target}/questions.yaml
+
+    # Replace # Include{serviceSelectorClusterIP} with the standard serviceSelectorClusterIP codesnippet
+    awk 'NR==FNR { a[n++]=$0; next }
+    /# Include{serviceSelectorClusterIP}/ { for (i=0;i<n;++i) print a[i]; next }
+    1' templates/questions/service/serviceSelectorClusterIP.yaml ${target}/questions.yaml > "tmp${chartname}" && mv "tmp${chartname}" ${target}/questions.yaml
+
+    # Replace # Include{serviceSelectorLoadBalancer} with the standard serviceSelectorLoadBalancer codesnippet
+    awk 'NR==FNR { a[n++]=$0; next }
+    /# Include{serviceSelectorLoadBalancer}/ { for (i=0;i<n;++i) print a[i]; next }
+    1' templates/questions/service/serviceSelectorLoadBalancer.yaml ${target}/questions.yaml > "tmp${chartname}" && mv "tmp${chartname}" ${target}/questions.yaml
+
+    # Replace # Include{serviceSelectorNodePort} with the standard serviceSelectorNodePort codesnippet
+    awk 'NR==FNR { a[n++]=$0; next }
+    /# Include{serviceSelectorNodePort}/ { for (i=0;i<n;++i) print a[i]; next }
+    1' templates/questions/service/serviceSelectorNodePort.yaml ${target}/questions.yaml > "tmp${chartname}" && mv "tmp${chartname}" ${target}/questions.yaml
 
     # Replace # Include{advancedPortHTTP} with the standard advancedPortHTTP codesnippet
     awk 'NR==FNR { a[n++]=$0; next }
