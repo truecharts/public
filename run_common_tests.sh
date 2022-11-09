d0ce7c478af2a99b61a749f6e9728be672d69f65
Stavros kois <s.kois@outlook.com>
2022-11-09 19:40:36 +0200

    use a maintained unittest

--- run_common_tests.sh
+++ run_common_tests.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# https://github.com/quintush/helm-unittest
 
+# -- You need to install this helm plugin
+# helm plugin install https://github.com/quintush/helm-unittest
 curr_dir=${pwd}
 
 common_test_path=library/common-test
 
 helm dependency build "$common_test_path"
 cd "$common_test_path"
-helm unittest -f "tests/*/*.yaml" .
+helm unittest --helm3 -f "tests/*/*.yaml" .
 cd "$curr_dir"
