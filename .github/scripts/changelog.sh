ff654486b3d869b0f4e03e087b470ba3c197080c
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-11-10 20:39:03 +0100

    feat(ci): move changelog directly to website (#4370)
    * feat(ci): move changelog directly to website
    
    * Put part of website changelog into SCALE App and add notice towards website
    
    * Don't push back into repo and combine helm-release jobs
    
    * force remove app-changelog

---
+++ .github/scripts/changelog.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ -d "charts/${1}" ]]; then
+    echo "Start processing charts/${1} ..."
+    chart="charts/${1}"
+    chartversion=$(cat charts/${1}/Chart.yaml | grep "^version: " | awk -F" " '{ print $2 }')
+    chartname=$(basename charts/${1})
+    train=$(basename $(dirname "charts/${1}"))
+    SCALESUPPORT=$(cat charts/${1}/Chart.yaml | yq '.annotations."truecharts.org/SCALE-support"' -r)
+    # Ensure to start with a clean slate
+    rm -rf ${chart}/app-changelog.md || echo "changelog not found..."
+    echo "Generating changelogs for: ${chartname}"
+    # SCALE "Changelog" containing only last change
+    git-chglog --next-tag ${chartname}-${chartversion} --tag-filter-pattern ${chartname} --path ${chart} -o ${chart}/app-changelog.md ${chartname}-${chartversion} || echo "changelog generation failed..."
+else
+    echo "Chart 'charts/${1}' no longer exists in repo. Skipping it..."
+fi
+echo "Done processing charts/${1} ..."
