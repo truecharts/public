e6a7acde9afd9235c8866e28cb5b6cb5be3e501c
Stavros kois <s.kois@outlook.com>
2022-11-20 22:41:55 +0200

    update common-test

--- helm_template_common.sh
+++ helm_template_common.sh
@@ -4,4 +4,5 @@ common_path=library/common/1.0.0
 
 go-yq -i '.type = "application"' "$common_path"/Chart.yaml
 helm template ./"$common_path"
+helm lint ./"$common_path"
 go-yq -i '.type = "library"' "$common_path"/Chart.yaml
