a07638a352bc381238afca76df8554aaf44990d3
Stavros kois <s.kois@outlook.com>
2022-12-11 16:00:52 +0200

    switch to helm install dryrun to run objects against kubeapi

--- helm_template_common.sh
+++ helm_template_common.sh
@@ -2,6 +2,10 @@
 
 chart_path=library/common-test
 
+if [ $1 == "-f" ] && [ ! -z $2 ]; then
+  extra_args=("-f" "$chart_path/ci/$2")
+fi
+
 if [ -d "$chart_path/charts" ]; then
   echo "Cleaning old built charts..."
   rm -r "$chart_path/charts"
@@ -11,5 +15,5 @@ fi
 echo "Building common..."
 helm dependency update "$chart_path"
 
-helm template --debug "./$chart_path"
+helm install --dry-run --debug common-test "${extra_args[@]}" "./$chart_path"
 helm lint "./$chart_path"
