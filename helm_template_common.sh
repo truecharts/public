d594f813430fed09e502c74bb0862d3babc5decf
Stavros kois <s.kois@outlook.com>
2022-12-12 20:27:35 +0200

    add cert tests

--- helm_template_common.sh
+++ helm_template_common.sh
@@ -2,8 +2,10 @@
 
 chart_path=library/common-test
 
-if [ $1 == "-f" ] && [ ! -z $2 ]; then
-  extra_args=("-f" "$chart_path/ci/$2")
+if [ ! $1 == "template" ]; then
+  if [ $1 == "-f" ] && [ ! -z $2 ]; then
+    extra_args=("-f" "$chart_path/ci/$2")
+  fi
 fi
 
 if [ -d "$chart_path/charts" ]; then
@@ -15,5 +17,11 @@ fi
 echo "Building common..."
 helm dependency update "$chart_path"
 
-helm install --dry-run --debug common-test "${extra_args[@]}" "./$chart_path"
+if [ $1 == "template" ]; then
+  echo "Running <helm template ./$chart_path"
+  helm template "./$chart_path"
+else
+  echo "Running <helm install --dry-run --debug common-test ${extra_args[@]} ./$chart_path"
+  helm install --dry-run --debug common-test "${extra_args[@]}" "./$chart_path"
+fi
 helm lint "./$chart_path"
