931ad330226017e6def080b4df0ee64d22b69544
Stavros kois <s.kois@outlook.com>
2022-12-28 21:19:34 +0200

    run all tests

--- run_common_tests.sh
+++ run_common_tests.sh
@@ -22,6 +22,6 @@ echo "🔨 Building common..."
 helm dependency update "$common_test_path"
 
 echo "🧪 Running tests..."
-helm unittest --update-snapshot --helm3 -f "tests/*init*/*.yaml" "./$common_test_path"
+helm unittest --update-snapshot --helm3 -f "tests/*/*.yaml" "./$common_test_path"
 
 cleanup
