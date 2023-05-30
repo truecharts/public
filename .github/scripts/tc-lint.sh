8b91e1e0def5a21c35a6ca0c950b84fbc2d817a9
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-05-30 13:54:44 +0300

    chore(ci): Add some checks to tc-lint.sh and update chart-tests to GITHUB_OUTPUT (#9289)
    **Description**
    <!--
    Please include a summary of the change and which issue is fixed. Please
    also include relevant motivation and context. List any dependencies that
    are required for this change.
    -->
    ⚒️ Fixes  # <!--(issue)-->
    
    **⚙️ Type of change**
    
    - [ ] ⚙️ Feature/App addition
    - [ ] 🪛 Bugfix
    - [ ] ⚠️ Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [x] 🔃 Refactor of current code
    
    **🧪 How Has This Been Tested?**
    <!--
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce. Please also list any relevant details
    for your test configuration
    -->
    
    **📃 Notes:**
    <!-- Please enter any other relevant information here -->
    
    **✔️ Checklist:**
    
    - [ ] ⚖️ My code follows the style guidelines of this project
    - [ ] 👀 I have performed a self-review of my own code
    - [ ] #️⃣ I have commented my code, particularly in hard-to-understand
    areas
    - [ ] 📄 I have made corresponding changes to the documentation
    - [ ] ⚠️ My changes generate no new warnings
    - [ ] 🧪 I have added tests to this description that prove my fix is
    effective or that my feature works
    - [ ] ⬆️ I increased versions for any altered app according to semantic
    versioning
    
    **➕ App addition**
    
    If this PR is an app addition please make sure you have done the
    following.
    
    - [ ] 🪞 I have opened a PR on
    [truecharts/containers](https://github.com/truecharts/containers) adding
    the container to TrueCharts mirror repo.
    - [ ] 🖼️ I have added an icon in the Chart's root directory called
    `icon.png`
    
    ---
    
    _Please don't blindly check all the boxes. Read them and only check
    those that apply.
    Those checkboxes are there for the reviewer to see what is this all
    about and
    the status of this PR with a quick glance._

--- .github/scripts/tc-lint.sh
+++ .github/scripts/tc-lint.sh
@@ -15,7 +15,7 @@ function check_version() {
     if [[ -n "$new" ]]; then
         echo -e "\t🔙 Old Chart Version: $old"
         echo -e "\t🆕 New Chart Version: $new"
-        
+
         if [[ $(echo "$new\n$old" | sort -V -r | head -n1) != "$old" ]]; then
             echo -e "\t✅ Chart version: Bumped"
         else
@@ -141,6 +141,33 @@ status_file="/tmp/status"
 exit_code=0
 
 result_file=${result_file:?"No result file provided"}
+
+rm -f "$status_file"
+rm -f "$status_file"
+
+command -v yamale >/dev/null 2>&1 || {
+    printf >&2 "%s\n" "yamale (https://github.com/23andMe/Yamale#pip) is not installed. Aborting."
+    printf >&2 "%s\n" "Install it with 'pip install yamale'"
+    exit 1
+}
+
+command -v yamllint >/dev/null 2>&1 || {
+    printf >&2 "%s\n" "yamllint (https://yamllint.readthedocs.io/en/stable/quickstart.html#installing-yamllint) is not installed. Aborting."
+    printf >&2 "%s\n" "Install it with 'pip install yamllint'"
+    exit 1
+}
+
+command -v helm >/dev/null 2>&1 || {
+    printf >&2 "%s\n" "helm (https://helm.sh/docs/intro/install) is not installed. Aborting."
+    printf >&2 "%s\n" "Install it with 'curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash'"
+    exit 1
+}
+
+command -v parallel >/dev/null 2>&1 || {
+    printf >&2 "%s\n" "parallel (https://www.gnu.org/software/parallel) is not installed. Aborting."
+    printf >&2 "%s\n" "Install it with 'sudo apt install parallel'"
+    exit 1
+}
 
 changed=$(echo $charts | jq --raw-output '.[]')
 
