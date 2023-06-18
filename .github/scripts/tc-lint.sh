a486c86f160835550f80187b7a22c0a5c9a9abfc
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-06-18 15:03:35 +0300

    cleanup

--- .github/scripts/tc-lint.sh
+++ .github/scripts/tc-lint.sh
@@ -7,7 +7,6 @@ function check_version() {
     # If only docs changed, skip version check
     # git diff target_branch, filter only on $chart_path and invert match for $chart_path/docs
     # note that it requires branches to be up to date for this to work.
-    git diff --name-status "$target_branch" -- "$chart_path" | grep -v "$chart_path/docs"
     chart_changes=$(git diff --name-status "$target_branch" -- "$chart_path" | grep -v "$chart_path/docs")
 
     if [[ -z "$chart_changes" ]]; then
