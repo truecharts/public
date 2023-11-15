f0b158b88c37fcded38cc579459f987ac9acceb7
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2023-11-15 15:29:58 +0100

    prep ci for moving over prometheus community

--- .github/scripts/fetch_helm_deps.sh
+++ .github/scripts/fetch_helm_deps.sh
@@ -119,6 +119,9 @@ for idx in $(eval echo "{0..$length}"); do
               elif [[ "$name" =~ "cloudnative-pg" ]]; then
                   helm dependency build "$charts_path/$train_chart/Chart.yaml" || \
                   helm dependency update "$charts_path/$train_chart/Chart.yaml"|| exit 1
+              elif [[ "$name" =~ "kube-prometheus-stack" ]]; then
+                  helm dependency build "$charts_path/$train_chart/Chart.yaml" || \
+                  helm dependency update "$charts_path/$train_chart/Chart.yaml"|| exit 1
               elif [[ "$name" =~ "cert-manager" ]]; then
                   helm dependency build "$charts_path/$train_chart/Chart.yaml" --verify --keyring $gpg_dir/certman.gpg || \
                   helm dependency update "$charts_path/$train_chart/Chart.yaml" --verify --keyring $gpg_dir/certman.gpg || exit 1
@@ -139,6 +142,8 @@ for idx in $(eval echo "{0..$length}"); do
                    echo "metallb is not signed..."
                 elif [[ "$name" =~ "cloudnative-pg" ]]; then
                    echo "cloudnative-pg is not signed..."
+                elif [[ "$name" =~ "kube-prometheus-stack" ]]; then
+                   echo "kube-prometheus-stack is not signed..."
                 elif [[ ! "$train_chart" =~ incubator\/.* ]]; then
                   echo "Validating dependency signature..."
                   helm verify $cache_path/$repo_dir/$name-$version.tgz --keyring $gpg_dir/pubring.gpg || \
@@ -159,6 +164,9 @@ for idx in $(eval echo "{0..$length}"); do
                   helm dependency build "$charts_path/$train_chart/Chart.yaml" || \
                   helm dependency update "$charts_path/$train_chart/Chart.yaml"|| exit 1
               elif [[ "$name" =~ "cloudnative-pg" ]]; then
+                  helm dependency build "$charts_path/$train_chart/Chart.yaml" || \
+                  helm dependency update "$charts_path/$train_chart/Chart.yaml"|| exit 1
+              elif [[ "$name" =~ "kube-prometheus-stack" ]]; then
                   helm dependency build "$charts_path/$train_chart/Chart.yaml" || \
                   helm dependency update "$charts_path/$train_chart/Chart.yaml"|| exit 1
               elif [[ "$name" =~ "cert-manager" ]]; then
