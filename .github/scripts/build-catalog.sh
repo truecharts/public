53845825907ef0a80b3f7b6ac4afbd4df691b42d
Stavros kois <s.kois@outlook.com>
2023-12-31 20:20:33 +0200

    chore(scripts): fix more

--- .github/scripts/build-catalog.sh
+++ .github/scripts/build-catalog.sh
@@ -96,7 +96,8 @@ patch_apps() {
         touch "website/docs/charts/${train}/${chartname}/CHANGELOG.md"
     fi
     cp -rf "website/docs/charts/${train}/${chartname}/CHANGELOG.md" "${target}/CHANGELOG.md" 2>/dev/null || :
-    if grep -q "for the complete changelog, please refer to the website" "${target}/CHANGELOG.md"; then
+    # If changelog is empty, add a notice
+    if ! grep -q "for the complete changelog, please refer to the website" "${target}/CHANGELOG.md"; then
         echo "Adding changelog notice for: ${chartname}"
         # Count the frontmatter lines
         line_count=$(sed -n '/^---$/,/^---$/p' "${target}/CHANGELOG.md" | wc -l)
