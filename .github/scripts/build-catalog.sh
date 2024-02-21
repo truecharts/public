3f82666c29d8e59fcb29b86fae199c88646d73b9
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2024-02-21 01:27:45 +0100

    fix(ci): correct mistake in CI

--- .github/scripts/build-catalog.sh
+++ .github/scripts/build-catalog.sh
@@ -3,8 +3,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-
-	
+patch_apps() {
 # Copy changelog from website
 if [[ ! -f "website/docs/charts/${train}/${chartname}/CHANGELOG.md" ]]; then
     touch "website/docs/charts/${train}/${chartname}/CHANGELOG.md"
@@ -24,5 +23,24 @@ fi
 echo "Truncating changelog for: ${chartname}"
 # Truncate changelog to only contain the last 100 lines
 sed -i '100,$ d' "${target}/CHANGELOG.md" || :
+}
+export -f patch_apps
+
+if [[ -d "charts/${1}" ]]; then
+    echo "Start processing charts/${1} ..."
+    chartversion=$(cat charts/${1}/Chart.yaml | grep "^version: " | awk -F" " '{ print $2 }')
+    chartname=$(basename charts/${1})
+    train=$(basename $(dirname "charts/${1}"))
+    SCALESUPPORT=$(cat charts/${1}/Chart.yaml | yq '.annotations."truecharts.org/SCALE-support"' -r)
+    if [[ "${SCALESUPPORT}" == "true" ]]; then
+        patch_apps "charts/${1}" "${chartname}" "$train" "${chartversion}"
+    else
+        echo "Skipping chart charts/${1}, no correct SCALE compatibility layer detected"
+    fi
+else
+    echo "Chart 'charts/${1}' no longer exists in repo. Skipping it..."
+fi
+echo "Done processing charts/${1} ..."
 	
+
 	
