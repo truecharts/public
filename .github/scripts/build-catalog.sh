1ee7093c5cdaee4a2873aff523e209b3ea48cbcf
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2024-02-21 01:05:01 +0100

    remove functions migrated to ChartTool

--- .github/scripts/build-catalog.sh
+++ .github/scripts/build-catalog.sh
@@ -3,20 +3,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-    # Generate screenshots
-    screenshots=""
-    if [[ -d "${target}/screenshots" ]]; then
-        screenshots=$(ls ${target}/screenshots)
-    fi
-    if [[ -n $screenshots ]]; then
-        echo "screenshots:" >> catalog/${train}/${chartname}/item.yaml
-        for screenshot in $screenshots; do
-            echo "  - https://truecharts.org/img/hotlink-ok/chart-screenshots/${chartname}/${screenshot}" >> catalog/${train}/${chartname}/item.yaml
-        done
-    else
-        echo "screenshots: []" >> catalog/${train}/${chartname}/item.yaml
-    fi
-    rm -rf ${target}/screenshots
+
+	
     # Copy changelog from website
     if [[ ! -f "website/docs/charts/${train}/${chartname}/CHANGELOG.md" ]]; then
         touch "website/docs/charts/${train}/${chartname}/CHANGELOG.md"
@@ -36,13 +24,5 @@ set -o pipefail
     echo "Truncating changelog for: ${chartname}"
     # Truncate changelog to only contain the last 100 lines
     sed -i '100,$ d' "${target}/CHANGELOG.md" || :
-    # Generate SCALE App description file
-    cat ${target}/Chart.yaml | yq .description -r >> ${target}/app-readme.md
-    echo "" >> ${target}/app-readme.md
-    echo "This App is supplied by TrueCharts, for more information visit the manual: [https://truecharts.org/charts/${train}/${chartname}](https://truecharts.org/charts/${train}/${chartname})" >> ${target}/app-readme.md
-    echo "" >> ${target}/app-readme.md
-    echo "---" >> ${target}/app-readme.md
-    echo "" >> ${target}/app-readme.md
-    echo "TrueCharts can only exist due to the incredible effort of our staff." >> ${target}/app-readme.md
-    echo "Please consider making a [donation](https://truecharts.org/sponsor) or contributing back to the project any way you can!" >> ${target}/app-readme.md
-    echo "app-readme generated"
+	
+	
