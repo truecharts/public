8c4639a7960299a33f2a4f8948269f3d51a76ed8
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2024-05-13 00:33:32 +0300

    reduce logging

--- .github/scripts/frontmatter.sh
+++ .github/scripts/frontmatter.sh
@@ -26,12 +26,12 @@ echo "Checking front matter"
 if ! grep -q "^---$" "$file_path"; then
   echo "Front matter (start) not found, adding it"
   # Dont trace content, as its usually too large
-  [ "$DEBUG" ] && set +x
+  [ "$DEBUG" == "true" ] && set +x
   content=$(cat "$file_path")
   echo -e "---\n" >"$file_path"
   echo -e "---\n" >>"$file_path"
   echo "$content" >>"$file_path"
-  [ "$DEBUG" ] && set -x
+  [ "$DEBUG" == "true" ] && set -x
 fi
 
 # Get the title from the front matter
