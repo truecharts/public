5940979f61f23d9a0ad9f81c6bbdf2a3be7e563d
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2024-05-13 01:54:35 +0300

    simpler

--- .github/scripts/frontmatter.sh
+++ .github/scripts/frontmatter.sh
@@ -4,23 +4,6 @@
 
 file_path="$1"
 base_cmd="go-yq --front-matter=process"
-# Check if the file has valid front matter
-
-is_empty() {
-  if $(echo "$1" | grep -q "^null$"); then
-    return 0
-  fi
-
-  return 1
-}
-
-is_true() {
-  if $(echo "$1" | grep -q "^true$"); then
-    return 0
-  fi
-
-  return 1
-}
 
 echo "Checking front matter"
 if ! head -n 1 "$file_path" | grep -q "^---$"; then
@@ -32,18 +15,10 @@ if ! head -n 1 "$file_path" | grep -q "^---$"; then
 fi
 
 wc -l $file_path
-cat $file_path
 
 # Get the title from the front matter
-echo "Checking title"
-title=$($base_cmd '.title' "$file_path")
-# Check if the title is empty
-if is_empty "$title"; then
-  $base_cmd -i '.title="Changelog"' "$file_path"
-fi
+echo "Updating title"
+$base_cmd -i '.title="Changelog"' "$file_path"
 
-echo "Checking pagefind"
-pagefind=$($base_cmd '.pagefind' "$file_path")
-if is_empty "$pagefind" || is_true "$pagefind"; then
-  $base_cmd -i '.pagefind=false' "$file_path"
-fi
+echo "Updating pagefind"
+$base_cmd -i '.pagefind=false' "$file_path"
