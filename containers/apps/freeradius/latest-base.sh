b7501e0aed708a74818fba738a869918c3e6c92f
Kjeld Schouten <info@kjeldschouten.nl>
2024-10-19 17:29:51 +0200

    chore(containers): BREAKING CHAINGE remove ubuntu (#28041)
    **Description**
    In favor of standardizing around Alpine where we can.
    
    **⚙️ Type of change**
    
    - [x] ⚙️ Feature/App addition
    - [ ] 🪛 Bugfix
    - [x] ⚠️ Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [x] 🔃 Refactor of current code
    
    **🧪 How Has This Been Tested?**
    <!--
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce. Please also list any relevant details
    for your test configuration
    -->
    
    **📃 Notes:**
    <!-- Please enter any other relevant information here -->
    
    **✔️ Checklist:**
    
    - [ ] ⚖️ My code follows the style guidelines of this project
    - [ ] 👀 I have performed a self-review of my own code
    - [ ] #️⃣ I have commented my code, particularly in hard-to-understand
    areas
    - [ ] 📄 I have made corresponding changes to the documentation
    - [ ] ⚠️ My changes generate no new warnings
    - [ ] 🧪 I have added tests to this description that prove my fix is
    effective or that my feature works
    - [ ] ⬆️ I increased versions for any altered app according to semantic
    versioning
    - [ ] I made sure the title starts with `feat(chart-name):`,
    `fix(chart-name):` or `chore(chart-name):`
    
    **➕ App addition**
    
    If this PR is an app addition please make sure you have done the
    following.
    
    - [ ] 🖼️ I have added an icon in the Chart's root directory called
    `icon.png`
    
    ---
    
    _Please don't blindly check all the boxes. Read them and only check
    those that apply.
    Those checkboxes are there for the reviewer to see what is this all
    about and
    the status of this PR with a quick glance._

--- containers/apps/freeradius/latest-base.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cat "./base/ubuntu/VERSION"
--- containers/apps/freeradius/latest-version.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-version=$(curl "https://registry.hub.docker.com/v1/repositories/freeradius/freeradius-server/tags" | jq --raw-output '.[].name' | grep -v layer | grep -v alpine | grep -v latest | tail -n1)
-version="${version#*v}"
-version="${version#*release-}"
-echo "${version}"
