7c3ecc54a4f903ad4cd7c6ff6735c1970389faf9
Kjeld Schouten <info@kjeldschouten.nl>
2024-11-07 00:46:54 +0100

    feat(clustertool): introduce tracing of command-slice and dont process extraArgs for now

--- clustertool/pkg/gencmd/apply.go
+++ clustertool/pkg/gencmd/apply.go
@@ -3,7 +3,6 @@ package gencmd
 import (
     "os"
     "path/filepath"
-    "strings"
 
     "github.com/rs/zerolog/log"
     "github.com/truecharts/public/clustertool/embed"
@@ -14,15 +13,13 @@ import (
 func GenApply(node string, extraArgs []string) []string {
 
     commands := []string{}
-    //extraFlags = append(extraFlags, "--preserve")
 
     talosPath := embed.GetTalosExec()
     if node == "" {
 
         for _, noderef := range talassist.TalConfig.Nodes {
-            // TODO add extraFlags
             filename := talassist.TalConfig.ClusterName + "-" + noderef.Hostname + ".yaml"
-            cmd := talosPath + " " + "apply-config" + " --talosconfig " + helper.TalosConfigFile + " -n " + noderef.IPAddress + " -f " + filepath.Join(helper.TalosGenerated, filename) + " " + strings.Join(extraArgs, " ")
+            cmd := talosPath + " " + "apply-config" + " --talosconfig " + helper.TalosConfigFile + " -n " + noderef.IPAddress + " -f " + filepath.Join(helper.TalosGenerated, filename) // + " " + strings.Join(extraArgs, " ")
             commands = append(commands, cmd)
         }
     } else {
@@ -38,7 +35,7 @@ func GenApply(node string, extraArgs []string) []string {
         }
 
         filename := talassist.TalConfig.ClusterName + "-" + nodename + ".yaml"
-        cmd := talosPath + " " + "apply-config" + " --talosconfig " + helper.TalosConfigFile + " -n " + node + " -f " + filepath.Join(helper.TalosGenerated, filename) + " " + strings.Join(extraArgs, " ")
+        cmd := talosPath + " " + "apply-config" + " --talosconfig " + helper.TalosConfigFile + " -n " + node + " -f " + filepath.Join(helper.TalosGenerated, filename) // + " " + strings.Join(extraArgs, " ")
         commands = append(commands, cmd)
     }
     log.Debug().Msgf("Apply Commands rendered: %s", commands)
