3ad410573493ab78f500086e961f35142b070467
Kjeld Schouten <info@kjeldschouten.nl>
2024-11-09 21:14:09 +0100

    fix(clustertool): (re-)enable extra args on plain cmd generator

--- clustertool/pkg/gencmd/plain.go
+++ clustertool/pkg/gencmd/plain.go
@@ -1,6 +1,8 @@
 package gencmd
 
 import (
+    "strings"
+
     "github.com/rs/zerolog/log"
     "github.com/truecharts/public/clustertool/embed"
     "github.com/truecharts/public/clustertool/pkg/helper"
@@ -15,11 +17,11 @@ func GenPlain(command string, node string, extraArgs []string) []string {
     if node == "" {
 
         for _, noderef := range talassist.TalConfig.Nodes {
-            cmd := talosPath + " " + command + " --talosconfig " + helper.TalosConfigFile + " -n " + noderef.IPAddress // + " " + strings.Join(extraArgs, " ")
+            cmd := talosPath + " " + command + " --talosconfig " + helper.TalosConfigFile + " -n " + noderef.IPAddress + " " + strings.Join(extraArgs, " ")
             commands = append(commands, cmd)
         }
     } else {
-        cmd := talosPath + " " + command + " --talosconfig " + helper.TalosConfigFile + " -n " + node // + " " + strings.Join(extraArgs, " ")
+        cmd := talosPath + " " + command + " --talosconfig " + helper.TalosConfigFile + " -n " + node + " " + strings.Join(extraArgs, " ")
         commands = append(commands, cmd)
     }
     log.Debug().Msgf("%s Command rendered: %s", command, commands)
