3f51c2ecb58581c1a00a417255c5be6b9d20919c
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2025-02-26 20:32:57 +0200

    fix

--- containers/apps/nextcloud-fpm/latest-version.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-version=$(cat ./apps/nextcloud-fpm/Dockerfile | grep "FROM public.ecr.aws/docker/library/nextcloud:" | cut -d ':' -f2 | cut -d '@' -f1 | cut -d '-' -f1)
-printf "%s" "${version}"
