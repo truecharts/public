4bd8ce5f8c229e41375cc2aa543adeeb8be116a3
Alfred Göppel <43101280+alfi0812@users.noreply.github.com>
2025-06-25 18:29:35 +0200

    fix(devcontainer): Make independent devcontainer & try to avoid version dependency (#36651)
    **Description**
    <!--
    Please include a summary of the change and which issue is fixed. Please
    also include relevant motivation and context. List any dependencies that
    are required for this change.
    -->
    ⚒️ Fixes  # <!--(issue)-->
    
    **⚙️ Type of change**
    
    - [ ] ⚙️ Feature/App addition
    - [ ] 🪛 Bugfix
    - [ ] ⚠️ Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] 🔃 Refactor of current code
    - [ ] 📜 Documentation Changes
    
    **🧪 How Has This Been Tested?**
    <!--
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce. Please also list any relevant details
    for your test configuration
    -->
    
    **📃 Notes:**
    <!-- Please enter any other relevant information here -->
    
    **✔️ Checklist:**
    
    - [ ] ⚖️ My code follows the style guidelines of this project
    - [ ] 👀 I have performed a self-review of my own code
    - [ ] #️⃣ I have commented my code, particularly in hard-to-understand
    areas
    - [ ] 📄 I have made changes to the documentation
    - [ ] 🧪 I have added tests to this description that prove my fix is
    effective or that my feature works
    - [ ] ⬆️ I increased versions for any altered app according to semantic
    versioning
    - [ ] I made sure the title starts with `feat(chart-name):`,
    `fix(chart-name):`, `chore(chart-name):`, `docs(chart-name):` or
    `fix(docs):`
    
    **➕ App addition**
    
    If this PR is an app addition please make sure you have done the
    following.
    
    - [ ] 🖼️ I have added an icon in the Chart's root directory called
    `icon.png`
    
    ---
    
    _Please don't blindly check all the boxes. Read them and only check
    those that apply.
    Those checkboxes are there for the reviewer to see what is this all
    about and
    the status of this PR with a quick glance._
    
    ---------
    
    Signed-off-by: Alfred Göppel <43101280+alfi0812@users.noreply.github.com>

--- clustertool/embed/generic/root/DOTREPLACEdevcontainer/postCreateCommand.sh
+++ clustertool/embed/generic/root/DOTREPLACEdevcontainer/postCreateCommand.sh
@@ -12,9 +12,18 @@ fisher install nickeb96/puffer-fish
 fisher install PatrickF1/fzf.fish
 "
 
-kubectl krew install pv-mounter
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
 
-# Create/update virtual environment
-if ! grep -q "venv /workspaces/" .venv/pyvenv.cfg; then
-    rm -rf .venv
-fi
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+kubectl krew install pv-mounter
+kubectl krew install cnpg
+kubectl krew install df-pv
--- containers/apps/devcontainer/latest-version.sh
+++
@@ -1,1 +0,0 @@
-curl -s "https://api.github.com/repos/truecharts/clustertool/releases/latest" | jq -r '.name' | sed 's/^clustertool-v//'
