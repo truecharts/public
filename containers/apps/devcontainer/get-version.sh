780dd3d02c9c584237a6c701e98f6a7f8fcf18da
Alfred Göppel <43101280+alfi0812@users.noreply.github.com>
2025-06-25 16:06:12 +0200

    Update get-version.sh
    Signed-off-by: Alfred Göppel <43101280+alfi0812@users.noreply.github.com>

--- containers/apps/devcontainer/get-version.sh
+++ containers/apps/devcontainer/get-version.sh
@@ -1,1 +1,1 @@
-curl -s "https://api.github.com/repos/truecharts/clustertool/releases/latest" | jq -r '.name' | sed 's/^clustertool-v//'
+curl -s "https://api.github.com/repos/truecharts/public/releases/latest" | jq -r '.name' | sed 's/^clustertool-v//'
